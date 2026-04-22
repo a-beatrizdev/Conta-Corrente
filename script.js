dfd30a21b0c3f65cec84bf80baeef67e15de4fd5
Alana <alanabeatriztobias@gmail.com>
2026-04-22 14:37:56 -0300

    Criando a função criarConta

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
 class Cliente {
 
-// Método construtor: executado quando criamos um novo cliente
+    // Método construtor: executado quando criamos um novo cliente
 
-constructor(nome,cpf,agencia,conta) {
+    constructor(nome, cpf, agencia, conta) {
 
-// "this" represemta o objeto que está sendo criado
+        // "this" represemta o objeto que está sendo criado
 
-this.nome = nome; // Armazena o nome recebido
-this.cpf = cpf; 
-this.agencia = agencia;
-this.conta = conta;
+        this.nome = nome; // Armazena o nome recebido
+        this.cpf = cpf;
+        this.agencia = agencia;
+        this.conta = conta;
 
-}
+    }
 
 }
 
@@ -21,38 +21,51 @@ this.conta = conta;
 
 class ContaCorrente {
 
-// Construtor recebe um cliente já criado
+    // Construtor recebe um cliente já criado
 
-constructor(cliente) {
+    constructor(cliente) {
 
-this.cliente = cliente // Associa a conta a um cliente
-this.saldo = 0; // Inicia o saldo com 0 
+        this.cliente = cliente // Associa a conta a um cliente
+        this.saldo = 0; // Inicia o saldo com 0 
+
+    }
+
+    // 2.1 Método para realizar o depósito
+    depositar(valor) {
+
+        // Guarda o saldo antes da operação 
+        let saldoAnterior = this.saldo;
+
+        // Soma o valo depositado ao saldo atual
+        this.saldo += valor;
+
+        return {
+
+            valor: valor, // Valor depositado
+            saldoAnterior: saldoAnterior, // Saldo antes da operação
+            saldoAtual: this.saldo, //Novo saldo após depósito
+            tipo: "Deposito", // Tipo da operação
+            data: new Date().tocolocaleString(), // Data atual formatada
+
+
+        };
+
+
+    }
+
+}
+
+// 3. Função principal (executada ao clicar no botão)
+
+function criarConta() {
+
+    // Pega o valor digitado no input "nome"
+
+    let nome = document.getElementById
+
+
+
+
 
 }
 
-// 2.1 Método para realizar o depósito
-depositar(valor) {
-
-// Guarda o saldo antes da operação 
-let saldoAnterior= this.saldo;
-
-// Soma o valo depositado ao saldo atual
-this.saldo += valor;
-
-return{
-
-valor:valor, // Valor depositado
-saldoAnterior:saldoAnterior // Saldo antes da operação
-saldoAtual:this.saldo, //Novo saldo após depósito
-tipo:"Deposito", // Tipo da operação
-data: new Date().tocolocaleString(), // Data atual formatada
-
-
-};
-
-
-}
-
-
-
-}
