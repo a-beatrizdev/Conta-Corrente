7bcb1656636c07130df0fac393f55e0261bf24b4
Alana <alanabeatriztobias@gmail.com>
2026-04-22 14:10:50 -0300

    Criando a classe ContaCorrente

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //1. Classe Cliente 
 
 class Cliente {
-    
 
+// Método construtor: executado quando criamos um novo cliente
 
+constructor(nome,cpf,agencia,conta){
 
+// "this" represemta o objeto que está sendo criado
 
-
+this.nome = nome; // Armazena o nome recebido
+this.cpf = cpf; 
+this.agencia = agencia;
+this.conta = conta;
 
 }
+
+}
+
